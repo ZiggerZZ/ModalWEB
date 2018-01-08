443cb9658fd7194d3b50c9f723d52f682a5ab3d8
ZiggerZZ <ziggerZZ@gmail.com>
2018-01-08 01:26:51 +0100

    All classes added, generate_quiz started

--- TD4/Outils4/bdd4.php
+++ TD4/Outils4/bdd4.php
@@ -131,3 +131,339 @@ class User {
     }
 
 }
+
+
+class Quiz {
+
+    public $quiz_id;
+    public $name;
+    public $number_of_hands;
+    public $average_rating;
+
+    public function __toString() {
+        $s = "[$this->quiz_id] $this->name $this->humber_of_hands $this->average_rating";
+    }
+
+    public static function getQuiz($dbh, $quiz_id) {
+        $query = "SELECT * FROM `Quiz` WHERE `quiz_id` = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Quiz');
+        $sth->execute(array($quiz_id));
+        if ($sth->rowCount() == 1) {
+            $quiz = $sth->fetch();
+            return $quiz;
+        } else {
+            return "La fonction getQuiz n'a pas renvoyé de résultat.";
+        }
+    }
+    
+    public static function createQuiz($dbh, $name, $number_of_hands) {
+        $query = "INSERT INTO `Quiz` (`name`, `number_of_hands`) VALUES (?, ?);";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Quiz');
+        $sth->execute(array($name,$number_of_hands));
+        $this->quiz_id = $dbh->lastInsertId();
+        
+    }
+    
+    /*public static function insertQuiz($dbh, $name, $number_of_hands) {
+        $sth = $dbh->prepare("SELECT * FROM `Quiz` WHERE `name` = ?");
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Quiz');
+        $sth->execute(array($name));
+        if ($sth->rowCount() == 0) {   // On teste si le login est déjà utilisé
+            $sth = $dbh->prepare("INSERT INTO `Quiz` (`name`, `number_of_hands`) VALUES (?, ?) VALUES(?, ?)");
+            $sth->execute(array($name, $number_of_hands));
+        }
+        else {echo "Attention le nom est déjà utilisé. La base de données n'a pas été modifiée.";}
+    }*/
+
+    public static function name($dbh,$quiz_id){
+        $sth = $dbh->prepare("SELECT `name` FROM `Quiz` WHERE `quiz_id`=?");
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Quiz');
+        $sth->execute(array($quiz_id));
+        return $sth->fetch()->name;
+    }
+
+    public function afficheliste($tab){
+        echo "<ul>";
+        foreach ($tab as $quiz){
+            echo "<li>";
+            echo($quiz);
+            echo "</li>";
+        }
+        echo "</ul>";
+        
+    }
+
+}
+
+class Hand {
+
+    public $hand_id;
+    public $quiz_id;
+    public $question;
+
+    public function __toString() {
+        $s = "[$this->quiz_id] $this->hand_id $this->question";
+    }
+
+    public static function getHand($dbh, $hand_id) {
+        $query = "SELECT * FROM `Hand` WHERE `hand_id` = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Hand');
+        $sth->execute(array($hand_id));
+        if ($sth->rowCount() == 1) {
+            $hand = $sth->fetch();
+            return $hand;
+        } else {
+            return "La fonction getHand n'a pas renvoyé de résultat.";
+        }
+    }
+    
+    public static function createHand($dbh, $quiz_id, $question) {
+        $query = "INSERT INTO `Hand` (`quiz_id`, `question`) VALUES (?, ?);";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Hand');
+        $sth->execute(array($quiz_id,$question));
+        
+    }
+    /*
+    public static function insertHand($dbh, $quiz_id, $question) {
+        $sth = $dbh->prepare("SELECT * FROM `Hand` WHERE `quiz_id` = ?");
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Hand');
+        $sth->execute(array($quiz_id));
+        //if ($sth->rowCount() == 0) {   // On teste si le login est déjà utilisé
+            $sth = $dbh->prepare("INSERT INTO `Hand` (`quiz_id`, `question`) VALUES (?, ?) VALUES(?, ?)");
+            $sth->execute(array($name, $number_of_hands));
+        //}
+        //else {echo "Attention le nom est déjà utilisé. La base de données n'a pas été modifiée.";}
+    }
+*/
+
+    public function afficheliste($tab){
+        echo "<ul>";
+        foreach ($tab as $hand){
+            echo "<li>";
+            echo($hand);
+            echo "</li>";
+        }
+        echo "</ul>";
+        
+    }
+
+}
+
+class Hand_Card {
+
+    public $hand_id;
+    public $card_id;
+
+    public function __toString() {
+        $s = "[$this->hand_id] $this->card_id";
+    }
+
+    public static function getHand_Card($dbh, $hand_id) {
+        $query = "SELECT * FROM `Hand_Card` WHERE `hand_id` = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Hand_Card');
+        $sth->execute(array($hand_id));
+        if ($sth->rowCount() == 13) {
+            $hand = $sth->fetch();
+            return $hand;
+        } else {
+            return "La fonction getHand_Card n'a pas renvoyé de résultat.";
+        }
+    }
+    
+    //?????????? ptet ya moyen de faire mieux
+    public static function createHand_Card($dbh, $hand_id, $array_card_id) {
+        $query = "INSERT INTO `Hand` (`quiz_id`, `question`) VALUES (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?);";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Hand_Card');
+        $sth->execute(array($hand_id,$array_card_id[0],$hand_id,$array_card_id[1],$hand_id,$array_card_id[2],$hand_id,$array_card_id[3],$hand_id,$array_card_id[4],$hand_id,$array_card_id[5],$hand_id,$array_card_id[6],$hand_id,$array_card_id[7],$hand_id,$array_card_id[8],$hand_id,$array_card_id[9],$hand_id,$array_card_id[10],$hand_id,$array_card_id[11],$hand_id,$array_card_id[12],$hand_id,$array_card_id[13]));       
+    }
+    
+    public function afficheliste($tab){
+        echo "<ul>";
+        foreach ($tab as $hand){
+            echo "<li>";
+            echo($hand);
+            echo "</li>";
+        }
+        echo "</ul>";
+        
+    }
+
+}
+
+
+class Answer {
+
+    public $answer_id;
+    public $user_id;
+    public $hand_id;
+    public $bid_id;
+    
+    public function __toString() {
+        $s = "[$this->answer_id] $this->user_id $this->hand_id $this->bid_id";
+    }
+
+    public static function getAnswer($dbh, $answer_id) {
+        $query = "SELECT * FROM `Answer` WHERE `answer_id` = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Answer');
+        $sth->execute(array($answer_id));
+        if ($sth->rowCount() == 1) {
+            $answer = $sth->fetch();
+            return $answer;
+        } else {
+            return "La fonction getAnswe n'a pas renvoyé de résultat.";
+        }
+    }
+    
+    public static function createAnswer($dbh, $user_id, $hand_id, $bid_id) {
+        $query = "INSERT INTO `Answer` (`user_id`, `hand_id`, `bid_id`) VALUES (?, ?, ?);";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Answer');
+        $sth->execute(array($user_id, $hand_id, $bid_id));
+        
+    }
+    
+    /*
+    public static function insertAnswer($dbh, $user_id, $hand_id, $bid_id) {
+        $sth = $dbh->prepare("SELECT * FROM `Answer` WHERE `hand_id` = ?");
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Answer');
+        $sth->execute(array($hand_id));
+        if ($sth->rowCount() == 0) {   // On teste si le hand_id est déjà utilisé
+            $sth = $dbh->prepare("INSERT INTO `Answer` (`user_id`, `hand_id`, `bid_id`) VALUES (?, ?, ?) VALUES(?, ?, ?)");
+            $sth->execute(array($user_id, $hand_id, $bid_id));
+        }
+        else {echo "Attention le nom est déjà utilisé. La base de données n'a pas été modifiée.";}
+    }*/
+
+
+    public function afficheliste($tab){
+        echo "<ul>";
+        foreach ($tab as $answer){
+            echo "<li>";
+            echo($answer);
+            echo "</li>";
+        }
+        echo "</ul>";
+        
+    }
+
+}
+
+
+class Share {
+
+    public $share_id;
+    public $user1_id;
+    public $user2_id;
+    public $quiz_id;
+
+    public function __toString() {
+        $s = "[$this->quiz_id] $this->share_id $this->user1_id $this->user2_id";
+    }
+
+    public static function getShare($dbh, $share_id) {
+        $query = "SELECT * FROM `Share` WHERE `share_id` = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Share');
+        $sth->execute(array($share_id));
+        if ($sth->rowCount() == 1) {
+            $share = $sth->fetch();
+            return $share;
+        } else {
+            return "La fonction getShare n'a pas renvoyé de résultat.";
+        }
+    }
+    
+    public static function createShare($dbh, $user1_id, $user2_id, $quiz_id) {
+        $query = "INSERT INTO `Quiz` (`user1_id`, `user2_id`, `quiz_id`) VALUES (?, ?, ?);";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Share');
+        $sth->execute(array($user1_id, $user2_id, $quiz_id));
+        
+    }
+    /*
+    public static function insertShare($dbh, $name, $number_of_hands) {
+        $sth = $dbh->prepare("SELECT * FROM `Share` WHERE `name` = ?");
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Quiz');
+        $sth->execute(array($name));
+        if ($sth->rowCount() == 0) {   // On teste si le login est déjà utilisé
+            $sth = $dbh->prepare("INSERT INTO `Quiz` (`name`, `number_of_hands`) VALUES (?, ?) VALUES(?, ?)");
+            $sth->execute(array($name, $number_of_hands));
+        }
+        else {echo "Attention le nom est déjà utilisé. La base de données n'a pas été modifiée.";}
+    }*/
+
+    public function afficheliste($tab){
+        echo "<ul>";
+        foreach ($tab as $share){
+            echo "<li>";
+            echo($share);
+            echo "</li>";
+        }
+        echo "</ul>";
+        
+    }
+
+}
+
+class Rating {
+
+    public $rating_id;
+    public $user_id;
+    public $quiz_id;
+    public $rating;
+
+    public function __toString() {
+        $s = "[$this->rating_id] $this->user_id $this->quiz_id $this->rating_id";
+    }
+
+    public static function getRating($dbh, $rating_id) {
+        $query = "SELECT * FROM `Rating` WHERE `rating_id` = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Rating');
+        $sth->execute(array($rating_id));
+        if ($sth->rowCount() == 1) {
+            $rating = $sth->fetch();
+            return $rating;
+        } else {
+            return "La fonction getRating n'a pas renvoyé de résultat.";
+        }
+    }
+    
+    public static function createRating($dbh, $user_id, $quiz_id, $rating) {
+        $query = "INSERT INTO `Quiz` (`user_id`, `quiz_id`, `rating`) VALUES (?, ?, ?);";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Rating');
+        $sth->execute(array($user_id, $quiz_id, $rating));
+        
+    }
+    /*
+    public static function insertShare($dbh, $name, $number_of_hands) {
+        $sth = $dbh->prepare("SELECT * FROM `Share` WHERE `name` = ?");
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Quiz');
+        $sth->execute(array($name));
+        if ($sth->rowCount() == 0) {   // On teste si le login est déjà utilisé
+            $sth = $dbh->prepare("INSERT INTO `Quiz` (`name`, `number_of_hands`) VALUES (?, ?) VALUES(?, ?)");
+            $sth->execute(array($name, $number_of_hands));
+        }
+        else {echo "Attention le nom est déjà utilisé. La base de données n'a pas été modifiée.";}
+    }*/
+
+    public function afficheliste($tab){
+        echo "<ul>";
+        foreach ($tab as $rating){
+            echo "<li>";
+            echo($rating);
+            echo "</li>";
+        }
+        echo "</ul>";
+        
+    }
+
+}
--- TD4/Outils4/generate_quiz.php
+++ TD4/Outils4/generate_quiz.php
@@ -1,17 +1,52 @@
-
 <html>
-<body>
+    <body>
 
-Min &spades; is <?php echo $_POST["min_spades"]; ?><br>
-Max &spades; is: <?php echo $_POST["max_spades"]; ?><br>
-<?php
-if (isset($_POST["inlineCheckbox2s"]))
-{
-    echo "User chose 2s ".$_POST["inlineCheckbox2s"];
-} else {
-    echo "User didn't choose 2s";
-}
-?>
+        <?php
+        session_start();
+        require_once 'Outils4/utils.php';
+        require_once 'Outils4/printForms.php';
+        require_once 'Outils4/bdd4.php';
 
-</body>
+        if (!isset($dbh)) {
+            $dbh = Database::connect();
+        }
+        
+        $number_of_hands = $_POST["number_of_hands"];
+        $name = $_POST["quiz_name"];
+        
+        $quiz = new Quiz();
+       
+        quiz::createQuiz($dbh, $name, $number_of_hands);
+
+        
+        //!!! We need quiz number !!! How can we get it? probably make $quiz_id = SELECT ... in createQuiz///
+        
+        $suits = [0 => "c", 1 => "d", 2 => "h", 3 => "s"];
+        $values = [1 => "2", 2 => "3", 3 => "4", 4 => "5", 5 => "6", 6 => "7", 7 => "8", 8 => "9",
+            9 => "10", 10 => "J", 11 => "Q", 12 => "K", 13 => "A"];
+
+        for ($i = 1; $i <= $number_of_hands; $i++) {
+
+            foreach ($suits as $suit_number => $suit) {
+                foreach ($values as $rang => $value) {
+                    if (isset($_POST[$value . $suit])) {
+                        
+                    }
+                }
+            }
+        }
+        ?>
+
+
+        Min &spades; is <?php echo $_POST["min_spades"]; ?><br>
+        Max &spades; is: <?php echo $_POST["max_spades"]; ?><br>
+        <?php
+        if (isset($_POST["2h"])) {
+            echo "User chose 2s " . $_POST["inlineCheckbox2h"];
+        } else {
+            echo "User didn't choose 2h";
+        }
+        ?>
+
+    </body>
 </html>
